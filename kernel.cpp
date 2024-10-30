b4067147edf748994198b034454d6b0087072fdf
RobertWesner <robert@wesner.io>
2024-10-30 18:39:41 +0100

    printf is increasingly complete and useful

--- kernel.cpp
+++ kernel.cpp
@@ -2,6 +2,9 @@
 
 int main()
 {
+    char c = 'x';
+
 //    printf("Hello World!\nI am printf()!\naaa\n%%\nThis should print F9A12058: %X\n%x\n---", 0xF9A12058, 0xA1998);
-    printf("%d\n%i\n%u", 123456, -1337, 1984);
+    // printf("%d\n%i\n%u", 123456, -1337, 1984);
+    printf("%p : %c\n%o", &c, c, 123123);
 }
