9afa73d76b24cc375c9d47f76139ea9b8837d480
RobertWesner <robert@wesner.io>
2024-10-30 17:57:42 +0100

    DECIMALS!

--- kernel.cpp
+++ kernel.cpp
@@ -2,5 +2,6 @@
 
 int main()
 {
-    printf("Hello World!\nI am printf()!\naaa\n%%\nThis should print F9A12058: %X\n%x\n---", 0xF9A12058, 0xA1998);
+//    printf("Hello World!\nI am printf()!\naaa\n%%\nThis should print F9A12058: %X\n%x\n---", 0xF9A12058, 0xA1998);
+    printf("%d\n%i\n%u", 123456, -1337, 1984);
 }
