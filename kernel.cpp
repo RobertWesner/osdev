70444e8c2732133a833ef0875bedd6f947e1bcd5
RobertWesner <robert@wesner.io>
2024-10-30 16:31:03 +0100

    more fiddling

--- kernel.cpp
+++ kernel.cpp
@@ -1,15 +1,6 @@
-void print()
-{
-    char* video_memory = (char*) 0xb8000;
+#include <cstdio>
 
-    // Create a pointer to a char , and point it to the first text cell of
-    // video memory ( i . e . the top - left of the screen )
-    // At the address pointed to by video_memory , store the character ’X ’
-    // ( i . e . display ’X ’ in the top - left of the screen ).
-    *video_memory = 'X';
-}
-
-int main ()
+int main()
 {
-  print();
+    printf();
 }
--- run.sh
+++ run.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 
 nasm boot_sect.asm -f bin -o boot_sect.bin
-nasm -f elf64 kernel_entry.asm -o kernel_entry.o
+nasm -f elf32 kernel_entry.asm -o kernel_entry.o
 
-g++ -ffreestanding -c kernel.cpp -o kernel.o
-ld -o kernel.bin -Ttext 0x1000 kernel_entry.o kernel.o --oformat binary
+g++ -m32 -fno-pie -ggdb3 -nostdinc -fno-builtin -I$(pwd)/lib -ffreestanding -c kernel.cpp -o kernel.o || exit
+ld -m elf_i386 -o kernel.bin -Ttext 0x1000 kernel_entry.o kernel.o --oformat binary
 
 cat boot_sect.bin kernel.bin > os-image.bin
 
-#Block device options:
-#-fda/-fdb file  use 'file' as floppy disk 0/1 image
-#-hda/-hdb file  use 'file' as hard disk 0/1 image
-#-hdc/-hdd file  use 'file' as hard disk 2/3 image
-#-cdrom file     use 'file' as CD-ROM image
-#qemu-system-x86_64 -fda boot_sect.bin
 qemu-system-x86_64  -drive file=os-image.bin,format=raw,index=0,media=disk
