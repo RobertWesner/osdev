8ce1d413e9d8bfade7316212a15c7a7bf5550c8d
RobertWesner <robert@wesner.io>
2024-10-30 15:30:32 +0100

    its C time!!

--- boot_sect.asm
+++ boot_sect.asm
@@ -1,4 +1,7 @@
 [org 0x7c00]
+KERNEL_OFFSET equ 0x1000
+
+    mov [BOOT_DRIVE], dl
 
     mov bp, 0x9000
     mov sp, bp
@@ -6,15 +9,28 @@
     mov bx, MSG_REAL_MODE
     call print
 
+    call load_kernel
+
     call switch_to_pm
 
-    ; isn't this jmp unnecessary?
-    jmp $
-
 %include "boot_16/print.asm"
+%include "boot_16/disk.asm"
 %include "gdt.asm"
 %include "boot_32/print.asm"
 %include "switch_to_pm.asm"
+
+[bits 16]
+
+load_kernel:
+    mov bx, MSG_LOAD_KERNEL
+    call print
+
+    mov bx, KERNEL_OFFSET
+    mov dh, 15
+    mov dl, [BOOT_DRIVE]
+    call disk_load
+
+    ret
 
 [bits 32]
 
@@ -22,11 +38,15 @@ BEGIN_PM:
     mov ebx, MSG_PROT_MODE
     call print32
 
+    call KERNEL_OFFSET
+
     jmp $
 
 ; GLOBALS
+BOOT_DRIVE              db 0
 MSG_REAL_MODE           db "Started in 16-bit Real Mode", 0
 MSG_PROT_MODE           db "Successfully landed in 32-bit Protected Mode", 0
+MSG_LOAD_KERNEL         db "Loading kernel into memory.", 0
 
 times 510-($-$$)        db 0
 dw 0xAA55
--- run.sh
+++ run.sh
@@ -2,10 +2,15 @@
 
 nasm boot_sect.asm -f bin -o boot_sect.bin
 
+gcc -ffreestanding -c kernel.c -o kernel.o
+ld -o kernel.bin -Ttext 0x1000 kernel.o --oformat binary
+
+cat boot_sect.bin kernel.bin > os-image.bin
+
 #Block device options:
 #-fda/-fdb file  use 'file' as floppy disk 0/1 image
 #-hda/-hdb file  use 'file' as hard disk 0/1 image
 #-hdc/-hdd file  use 'file' as hard disk 2/3 image
 #-cdrom file     use 'file' as CD-ROM image
 #qemu-system-x86_64 -fda boot_sect.bin
-qemu-system-x86_64  -drive file=boot_sect.bin,format=raw,index=0,media=disk
+qemu-system-x86_64  -drive file=os-image.bin,format=raw,index=0,media=disk
